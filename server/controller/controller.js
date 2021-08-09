3742f551740effc5d35ae123c232b142aa92f28b
Jayasree P R <jayasree.pr04@gmail.com>
2021-08-09 23:07:19 +0530

    Update controller.js

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -28,23 +28,6 @@ user
 
 exports.find = (req,res) => {
 
-   /* if(req.query.id){
-        const id= req.query.id;
-        Userdb.findById(id)
-        .then(data=>{
-            if(!data){
-                res.status(404).send({ message : "Not found user with id "+ id})
-            }
-            else{
-            res.send(data)
-        }
-    })
-    .catch(err =>{
-        res.status(500).send({ message: "Error retrieving user with id " + id})
-    })
-
-    }
-    else{*/
 
 Userdb.find()
     .then(user=>{
